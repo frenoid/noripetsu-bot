7827077b0d50a9225b86ed01486f566470926d50
Norman Lim <frenoid@gmail.com>
2021-07-16 17:07:42 +0000

    change message to effective_message in app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def respond():
    # retrieve the message in JSON and then transform it to Telegram object
    update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-   chat_id = update.message.chat.id
-   msg_id = update.message.message_id
+   chat_id = update.effective_message.chat.id
+   msg_id = update.effective_message.message_id
 
    time_mod_10 = int(time()) % 10
 
