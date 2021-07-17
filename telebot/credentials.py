2e5e4e3fbb734472386d736931df6b78074cfe0f
Norman Lim <frenoid@gmail.com>
2021-07-17 15:04:11 +0000

    add aws key credentials to credentials.py

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -3,3 +3,5 @@ import os
 bot_token = os.environ["BOT_TOKEN"]
 bot_user_name = os.environ["BOT_USER_NAME"]
 URL = os.environ["URL"]
+aws_access_key_id = os.environ["AWS_ACCESS_KEY_ID"]
+aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"]
